00a7acdbb5a5cc25b5268f92b7346501e4638a17
Winniebran <winnie_brandao@yahoo.com.br>
2023-01-11 17:46:08 -0300

    feat: add updateUsers controller and service

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUsersController } from "../controllers/user/createUsers.controller";
 import { listUsersController } from "../controllers/user/listUsers.controller";
+import { updateUsersController } from "../controllers/user/updateUsers.controller";
 import { dataIsValidMiddleware } from "../middlewares/dataIsValid.middleware";
 import { createUsersSchema } from "../schemas/users.schema";
 
@@ -12,4 +13,6 @@ usersRouter.post(
   createUsersController
 );
 
-usersRouter.get("", listUsersController) //falta middlewares
+usersRouter.get("", listUsersController) //faltam middlewares
+
+usersRouter.patch("/:id", updateUsersController) //faltam middlewares
