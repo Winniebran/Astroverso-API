5d3289010be06ed0832c6f4824a687ff5dbf440a
Winniebran <winnie_brandao@yahoo.com.br>
2023-01-11 17:24:45 -0300

    fix: server running

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,20 @@ import "reflect-metadata";
 import { handleError } from "./errors/handleError";
 import { usersRouter } from "./routes/users.routes";
 
+
 export const app = express();
 app.use(express.json());
 
 app.use("/users", usersRouter);
-app.use("/login", );
-app.use("/favoritePosts", );
-app.use("/posts", );
-app.use("/astros", );
-app.use("/categories", );
-app.use("/quiz", );
-app.use("/questions", );
-app.use("/options", );
-app.use("/extras", );
-app.use("/types", );
+// app.use("/login");
+// app.use("/favoritePosts");
+// app.use("/posts");
+// app.use("/astros");
+// app.use("/categories");
+// app.use("/quiz");
+// app.use("/questions");
+// app.use("/options");
+// app.use("/extras");
+// app.use("/types");
 
 app.use(handleError);
