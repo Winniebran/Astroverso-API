ce51a3c4fc2e44f0c45ed7e10608d3941f0a408e
Winniebran <winnie_brandao@yahoo.com.br>
2023-01-12 18:45:23 -0300

    feat: GET /users -  Must be able to list users

---
+++ src/__tests__/integration/user/usersRoutes.test.ts
@@ -0,0 +1,35 @@
+import { DataSource } from "typeorm";
+import dataSourceConfig from "../../../data-source";
+import request from "supertest";
+import { app } from "../../../app";
+import { mockAdm, mockAdmLogin } from "../../mocks";
+
+describe("/users", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    await dataSourceConfig
+      .initialize()
+      .then((res) => {
+        connection = res;
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("GET /users -  Must be able to list users", async () => {
+    await request(app).post("/users").send(mockAdm);
+    const admLogin = await request(app).post("/login").send(mockAdmLogin);
+    const response = await request(app)
+      .get("/users")
+      .set("Authorization", `Bearer ${admLogin.body.token}`);
+
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0]).not.toHaveProperty("password");
+  });
+});
