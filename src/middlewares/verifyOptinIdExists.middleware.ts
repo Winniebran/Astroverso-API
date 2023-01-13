f584ff5ebe41b806c6f9d4e3bf08755f4c9f4352
FilipeC <filipecostasil77@gmail.com>
2023-01-13 09:11:33 -0300

    add/ deleteOptionsRoute

---
+++ src/middlewares/verifyOptinIdExists.middleware.ts
@@ -0,0 +1,26 @@
+import { Response, Request, NextFunction } from "express";
+import DataSource from "../data-source";
+import { Options } from "../entities/options.entity";
+import { AppError } from "../errors/AppErrors";
+
+export const verifyOptionsExistsMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const myTable = DataSource.getRepository(Options);
+
+    const verify = myTable.findOneBy({
+      id: req.body.id,
+    });
+
+    if (!verify) {
+      throw new AppError("Option id not exists");
+    }
+
+    next();
+  } catch (error) {
+    throw new AppError(error as string);
+  }
+};
--- src/routes/options.routes.ts
+++ src/routes/options.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { createOptionsController } from "../controllers/options/createOptions.controller";
 import verifyCorrectOptionsMiddleware from "../middlewares/verifyIsCorrectOptions.middleware";
+import { postOptionsSchema } from "../schemas/options.schema";
+import { verifyOptionsExistsMiddleware } from "../middlewares/verifyOptinIdExists.middleware";
+import { deleteOptionController } from "../controllers/options/deleteOptions.controller";
 
 const optionsRouter = Router();
 
 optionsRouter.post("", verifyCorrectOptionsMiddleware, createOptionsController);
+
+optionsRouter.delete(
+  "/:id",
+  verifyOptionsExistsMiddleware,
+  deleteOptionController
+);
 
 export default optionsRouter;
