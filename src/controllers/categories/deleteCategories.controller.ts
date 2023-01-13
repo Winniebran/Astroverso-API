89fc8649ed53160731373a9513fbe712860d34e2
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-01-13 13:49:42 -0300

    feat: add files and code to the categories resource

---
+++ src/controllers/categories/deleteCategories.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { deleteCategoriesService } from "../../services/categories/deleteCategories.service";
+
+export const deleteCategoriesController = async (
+	req: Request,
+	res: Response
+) => {
+	/*const userReqIsAdm = req.user.isAdm;*/
+	const categoryId = req.params.id;
+	await deleteCategoriesService(categoryId);
+
+	return res.status(204).json();
+};
---
+++ src/services/categories/listCategories.service.ts
@@ -0,0 +1,17 @@
+import dataSourceConfig from "../../data-source";
+import { Categories } from "../../entities/categories.entity";
+import { ICategoriesResponse } from "../../interfaces/categories";
+import { listCategoriesSchema } from "../../schemas/categories.schema";
+
+export const listCategoriesService = async (): Promise<
+	ICategoriesResponse[] | undefined
+> => {
+	const categoriesRepository = dataSourceConfig.getRepository(Categories);
+	const listCategories = await categoriesRepository.find();
+
+	const validatedData = await listCategoriesSchema.validate(listCategories, {
+		stripUnknown: true
+	});
+
+	return validatedData;
+};
