93a74485e830feca144631e68be03079791386b1
FilipeC <filipecostasil77@gmail.com>
2023-01-13 14:40:05 -0300

    fix/ yupTypesForOptions

--- src/controllers/options/updateOptions.controller.ts
+++ src/controllers/options/updateOptions.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { IOptions } from "../../interfaces/options";
+import { IupdateOption } from "../../interfaces/options";
 import { updateOptionsService } from "../../services/options/updateOptionsService";
 
 export const updateOptionsController = async (req: Request, res: Response) => {
-  const optionBody: IOptions = req.body;
+  const optionBody: IupdateOption = req.body;
   const id: string = req.params.id;
   const data = updateOptionsService(optionBody, id);
   return res.status(200).json(data);
--- src/schemas/options.schema.ts
+++ src/schemas/options.schema.ts
@@ -1,15 +1,17 @@
+import { IupdateOption } from "./../interfaces/options/index";
 import * as yup from "yup";
+import { SchemaOf } from "yup";
+import { IOptions } from "../interfaces/options";
 
-export const postOptionsSchema = yup.object().shape({
+export const postOptionsSchema: SchemaOf<IOptions> = yup.object().shape({
   answer: yup.string().required(),
   point: yup.number().required(),
   isCorrect: yup.boolean(),
-  questionsId: yup.number().required(),
+  questionsId: yup.string().required(),
 });
 
-export const updateOptionsSchema = yup.object().shape({
+export const updateOptionsSchema: SchemaOf<IupdateOption> = yup.object().shape({
   answer: yup.string(),
   point: yup.number(),
   isCorrect: yup.boolean(),
-  questionsId: yup.number(),
 });
