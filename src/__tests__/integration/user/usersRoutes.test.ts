f6b274a46c5536bb91f330782c42ac690f78bab1
Winniebran <winnie_brandao@yahoo.com.br>
2023-01-13 15:05:17 -0300

    feat: POST /users -  Must be able to create a user

--- src/__tests__/integration/user/usersRoutes.test.ts
+++ src/__tests__/integration/user/usersRoutes.test.ts
@@ -2,7 +2,13 @@ import { DataSource } from "typeorm";
 import dataSourceConfig from "../../../data-source";
 import request from "supertest";
 import { app } from "../../../app";
-import { mockAdm, mockAdmLogin, mockUser, mockUserLogin } from "../../mocks/users.mocks";
+import {
+  mockAdm,
+  mockAdmLogin,
+  mockUser,
+  mockUser2,
+  mockUserLogin,
+} from "../../mocks/users.mocks";
 
 describe("/users", () => {
   let connection: DataSource;
@@ -25,18 +31,18 @@ describe("/users", () => {
   test("GET /users -  Must be able to list users", async () => {
     await request(app).post("/users").send(mockAdm);
     const admLogin = await request(app).post("/login").send(mockAdmLogin);
-    const response = await request(app)
+    const res = await request(app)
       .get("/users")
       .set("Authorization", `Bearer ${admLogin.body.token}`);
 
-    expect(response.body).toHaveLength(1);
-    expect(response.body[0]).not.toHaveProperty("password");
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0]).not.toHaveProperty("password");
   });
 
   test("GET /users -  shouldn't be able to list users without authentication", async () => {
-    const response = await request(app).get("/users");
-    expect(response.status).toBe(401);
-    expect(response.body).toHaveProperty("message");
+    const res = await request(app).get("/users");
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty("message");
   });
 
   test("GET /users -  shouldn't be able to list users not being admin", async () => {
@@ -51,4 +57,35 @@ describe("/users", () => {
 
   // GET /users/:id/favoritePosts
 
+  //   test("GET /users/:id/favoritePosts -  shouldn't be able to list favorite posts without authentication", async () => {
+  //     const userLogin = await request(app).post("/login").send(mockUserLogin);
+  //     await request(app)
+  //       .get("/users")
+  //       .set("Authorization", `Bearer ${userLogin.body.token}`);
+  //     const res = await request(app).get(
+  //       `/users/${userLogin.body[0].id}/favoritePosts`
+  //     );
+  //     expect(res.status).toBe(401);
+  //     expect(res.body).toHaveProperty("message");
+  //   });
+
+  // POST /users
+
+  test("POST /users -  Must be able to create a user", async () => {
+    const response = await request(app).post("/users").send(mockUser2);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("isAdm");
+    expect(response.body).toHaveProperty("score");
+    expect(response.body).toHaveProperty("isActive");
+    expect(response.body).toHaveProperty("createdAt");
+    expect(response.body).toHaveProperty("updatedAt");
+    expect(response.body).not.toHaveProperty("password");
+    expect(response.body.name).toEqual("João");
+    expect(response.body.email).toEqual("joao@mail.com");
+    expect(response.body.isAdm).toEqual(false);
+    expect(response.status).toBe(201);
+  });
 });
