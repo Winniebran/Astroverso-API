a606a4ddab34aad4dbbb3b6f241c4dbecbe138b6
FilipeC <filipecostasil77@gmail.com>
2023-01-14 20:14:50 -0300

    add/manyTest

---
+++ src/__tests__/mocks/options.mock.ts
@@ -0,0 +1,45 @@
+import { IOptions, IupdateOption } from "../../interfaces/options";
+
+export const mockCreateOptionTrue: IOptions = {
+  answer: "option 1",
+  point: 2,
+  questionsId: "",
+  isCorrect: true,
+};
+
+export const mockCreateOptionFalse01: IOptions = {
+  answer: "option 2",
+  point: 0,
+  questionsId: "",
+};
+
+export const mockCreateOptionFalse02: IOptions = {
+  answer: "option 3",
+  point: 0,
+  questionsId: "",
+};
+
+export const mockCreateOptionFalse03: IOptions = {
+  answer: "option 4",
+  point: 0,
+  questionsId: "",
+};
+
+export const mockCantCreateMoreOption: IOptions = {
+  answer: "option 5",
+  point: 0,
+  questionsId: "",
+};
+
+export const mockCantCreateOptionTrueByPointEqual0: IOptions = {
+  answer: "option 1",
+  point: 0,
+  questionsId: "",
+  isCorrect: true,
+};
+
+export const mockCantCreateOptionFalseByPointEqual2: IOptions = {
+  answer: "option 1",
+  point: 2,
+  questionsId: "",
+};
