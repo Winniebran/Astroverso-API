75cf56db78342f63d6b8f8d2b59d8895a9aaa94f
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-01-14 01:09:46 -0300

    feat: add code to astroRoutes.test.ts

---
+++ src/__tests__/mocks/astros.mocks.ts
@@ -0,0 +1,11 @@
+import { IAstrosRequest, IAstrosUpdate } from "../../interfaces/astros";
+
+export const mockAstro: IAstrosRequest = {
+	name: "Galaxies",
+	image:
+		"https://www.visualcapitalist.com/wp-content/uploads/2022/06/The-entire-universe-1000x600.jpeg"
+};
+
+export const mockAstroUpdate: IAstrosUpdate = {
+	name: "AstroUpdate"
+};
