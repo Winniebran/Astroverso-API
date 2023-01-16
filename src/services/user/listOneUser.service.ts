ee3eb63f6135697a63a361996111f53b45dab944
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-01-16 20:51:08 -0300

    fix: add idIsValid middleware in listOneUser router

--- src/services/user/listOneUser.service.ts
+++ src/services/user/listOneUser.service.ts
@@ -2,6 +2,7 @@ import { IUserResponse } from "../../interfaces/user";
 import dataSourceConfig from "../../data-source";
 import { Users } from "../../entities/users.entity";
 import { listUsersWithoutPassword } from "../../schemas/users.schema";
+import { AppError } from "../../errors/AppErrors";
 
 export const listOneUserService = async (
   id: string
@@ -12,7 +13,11 @@ export const listOneUserService = async (
     where: { id: id },
     withDeleted: true,
   });
-  
+
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
   const userWithoutPassword = await listUsersWithoutPassword.validate(user, {
     stripUnknown: true,
   });
