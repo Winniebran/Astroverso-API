55d9c1f2010947d65de22713154f9c1886bb39b4
Lucas <lucas-leite-menezes@hotmail.com>
2023-01-16 12:42:27 -0300

    feat: add tests

--- src/controllers/quiz/updateQuiz.controller.ts
+++ src/controllers/quiz/updateQuiz.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { IQuizzesUpdate } from "../../interfaces/quizzes";
+import { IQuizzes } from "../../interfaces/quizzes";
 import { updateQuizService } from "../../services/quiz/updateQuiz.service";
 
 export const updateQuizController = async (req: Request, res: Response) => {
   const id: string = req.params.id;
-  const quizData: IQuizzesUpdate = req.body;
+  const quizData: IQuizzes = req.body;
   const updatedQuiz = await updateQuizService(id, quizData);
   return res.status(201).json(updatedQuiz);
 };
