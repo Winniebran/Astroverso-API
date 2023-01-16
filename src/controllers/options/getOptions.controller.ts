f71eaddea7f9c3117ca4628ac66385147e52b168
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-01-16 18:46:27 -0300

    fix: change logic and add code pattern

--- src/controllers/options/getOptions.controller.ts
+++ src/controllers/options/getOptions.controller.ts
@@ -1,7 +1,7 @@
-import { getOptionsSrvice } from "./../../services/options/getOptions.service";
+import { getOptionsService } from "./../../services/options/getOptions.service";
 import { Request, Response } from "express";
 
 export const getOptionsController = async (req: Request, res: Response) => {
-	const data = await getOptionsSrvice();
-	return res.status(200).json(data);
+  const data = await getOptionsService();
+  return res.status(200).json(data);
 };
--- src/interfaces/options/index.ts
+++ src/interfaces/options/index.ts
@@ -1,8 +1,17 @@
+import { IQuestionsResponse } from "../questions";
+
 export interface IOptions {
 	answer: string;
 	point: number;
-	isCorrect?: boolean;
+	isCorrect: boolean;
 	questionsId: string;
+}
+
+export interface IOptionsResponse {
+	answer: string;
+	point: number;
+	isCorrect: boolean;
+	questions: IQuestionsResponse;
 }
 
 export interface IUpdateOption {
