bb5acf50a0c1391105008fd2a3575d2979388665
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-01-17 09:46:27 -0300

    fix: remove from files code unused

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,35 +4,35 @@ import "dotenv/config";
 import "reflect-metadata";
 
 const setDataSourceConfig = (): DataSourceOptions => {
-  if (process.env.NODE_ENV === "production") {
-    return {
-      type: "postgres",
-      url: process.env.DATABASE_URL,
-      entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-      migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
-    };
-  }
+	if (process.env.NODE_ENV === "production") {
+		return {
+			type: "postgres",
+			url: process.env.DATABASE_URL,
+			entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+			migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
+		};
+	}
 
-  if (process.env.NODE_ENV === "test")
-    return {
-      type: "sqlite",
-      database: ":memory:",
-      synchronize: true,
-      entities: ["src/entities/*.ts"],
-    };
+	if (process.env.NODE_ENV === "test")
+		return {
+			type: "sqlite",
+			database: ":memory:",
+			synchronize: true,
+			entities: ["src/entities/*.ts"]
+		};
 
-  return {
-    type: "postgres",
-    host: process.env.PGHOST,
-    port: parseInt(process.env.PGPORT!),
-    username: process.env.PGUSER,
-    password: 'julitha123',
-    database: process.env.PGDATABASE,
-    logging: true,
-    synchronize: false,
-    entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-    migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
-  };
+	return {
+		type: "postgres",
+		host: process.env.PGHOST,
+		port: parseInt(process.env.PGPORT!),
+		username: process.env.PGUSER,
+		password: process.env.PGPASSWORD,
+		database: process.env.PGDATABASE,
+		logging: true,
+		synchronize: false,
+		entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+		migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
+	};
 };
 
 const dataSourceConfig = setDataSourceConfig();
